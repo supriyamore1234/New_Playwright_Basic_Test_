18636c5af17b23041f7d25523de7a308533ee3d4
Your Name <moresupriyasunil@gmail.com>
2025-04-26 14:59:15 +0530

    your commit message

---
+++ tests/Hooks2.spec.js
@@ -0,0 +1,33 @@
+import{test,expect} from  '@playwright/test';
+let page
+test.beforeAll(async({browser})=>{
+  page=await browser.newPage();
+await page.goto('https://www.demoblaze.com/index.html')
+//login
+await page.locator('#login2').click();
+await page.locator('#loginusername').fill('test');
+await page.locator('#loginpassword').fill('test');
+await page.locator('//button[normalize-space()="Log in"]').click();
+})
+
+test('Home page test',async()=>{const products=await page.$$('.card-block');
+  expect(products).not.toBeNull();
+  const productsLength=products.length;
+  expect(products).toHaveLength(productsLength);
+  
+})
+
+test.afterAll(async()=>{
+  await page.locator('#logout2').click();
+  console.log("Logout test");
+})
+
+test('Add product to cart test',async()=>{
+await page.locator("//a[text()='Samsung galaxy s6']").click();
+await page.locator("//a[text()='Add to cart']").click();
+//logout
+page.on('dialog',async dialog=> {
+  expect(dialog.message()).toContain('Product added');
+  await dialog.accept();
+})
+})
